3c30dacf22e753f64ca79af62ce88443b8d8e3e4
gitlab+deploy-token-4090191 <pythonlhu@gmail.com>
2025-02-27 06:32:27 +1100

    finla

--- obj/Debug/net8.0/TodoGrpc.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoGrpc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoGrpc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9381f684d5434b12238ec50ff0c5596607ac8889")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoGrpc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoGrpc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
